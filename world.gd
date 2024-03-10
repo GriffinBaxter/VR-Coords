e011abde4854e7ce57de28996336eb018761f86b
Griffin Baxter <GriffinRBaxter@gmail.com>
2024-03-10 18:31:33 +1300

    GAME-31: Remove unused script

--- world.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-var interface : XRInterface
-
-func _ready():
-	print("test")
-	interface = XRServer.find_interface("OpenXR")
-	if interface and interface.is_initialized():
-		get_viewport().use_xr = true
